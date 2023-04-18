b4aad2a1e3320e73fd930825e81fdf887a98c402
sauvageb <sauvageboris.pro@gmail.com>
2023-04-18 10:20:17 +0200

    feat(ConnectionManager): create singleton for jdbc connection

--- src/main/java/com/example/demo/dao/ConnectionManager.java
+++ src/main/java/com/example/demo/dao/ConnectionManager.java
@@ -1,2 +1,34 @@
-package com.example.demo.dao;public class ConnectionManager {
+package com.example.demo.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public final class ConnectionManager {
+
+    private static Connection INSTANCE;
+    private static final String url = "jdbc:mysql://localhost:8889/posts";
+    private static final String user = "root";
+    private static final String password = "root";
+
+    private ConnectionManager() {
+    }
+
+    public static Connection getInstance() {
+        if (INSTANCE == null) {
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                INSTANCE = DriverManager.getConnection(url, user, password);
+            } catch (SQLException e) {
+                System.err.println("Error during getConnection");
+                e.printStackTrace();
+                throw new RuntimeException(e);
+            } catch (ClassNotFoundException e) {
+                System.err.println("Error during loading driver");
+                e.printStackTrace();
+                throw new RuntimeException(e);
+            }
+        }
+        return INSTANCE;
+    }
 }
